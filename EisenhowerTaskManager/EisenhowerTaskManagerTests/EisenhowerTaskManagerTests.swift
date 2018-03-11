ecf39eccb68ad1cb2ec799e9f9bcda252bee4aea
Oleg Gorbatchev <oleg.gorbatchev@gmail.com>
2018-03-11 20:01:30 +0100

    [SAVE] Bitrise integration

--- EisenhowerTaskManager/EisenhowerTaskManagerTests/EisenhowerTaskManagerTests.swift
+++ EisenhowerTaskManager/EisenhowerTaskManagerTests/EisenhowerTaskManagerTests.swift
@@ -2,19 +2,25 @@
 //  EisenhowerTaskManagerTests.swift
 //  EisenhowerTaskManagerTests
 //
-//  Created by Oleg GORBATCHEV on 26/02/2018.
+//  Created by Oleg GORBATCHEV on 11/03/2018.
 //  Copyright © 2018 Oleg Gorbatchev. All rights reserved.
 //
 
 import XCTest
-@testable import EisenhowerTaskManager
 
 class EisenhowerTaskManagerTests: XCTestCase {
     override func setUp() {
         super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 }
