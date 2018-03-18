b8fb7fcda90b586ad205e9cec9ff15abf2fe48d4
ake <ake@witbe.net>
2018-03-18 16:19:54 +0100

    [ADD] Added basic settings

---
+++ EisenhowerTaskManager/EisenhowerTaskManager/Views/SettingsVC/ProfileVC/ProfileVC.swift
@@ -0,0 +1,56 @@
+//
+//  ProfileVC.swift
+//  EisenhowerTaskManager
+//
+//  Created by Samy Kettani on 14/03/2018.
+//  Copyright © 2018 Oleg Gorbatchev. All rights reserved.
+//
+
+import UIKit
+import Eureka
+import GenericPasswordRow
+import FirebaseAuth
+
+class ProfileVC: FormViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		self.title = "Profile"
+
+		form +++ Section("Update password")
+			<<< GenericPasswordRow {
+				$0.tag = "newpwd1"
+			}
+			<<< GenericPasswordRow {
+				$0.tag = "newpwd2"
+			}
+			<<< ButtonRow {
+				$0.title = "Change password"
+				$0.onCellSelection { _, _ in
+					self.changePassword()
+				}
+		}
+    }
+}
+
+extension ProfileVC {
+	private func changePassword() {
+		print("Hello there")
+
+		let pwdRow1: GenericPasswordRow? = form.rowBy(tag: "newpwd1")
+		let pwd1 = pwdRow1?.value
+		let pwdRow2: GenericPasswordRow? = form.rowBy(tag: "newpwd2")
+		let pwd2 = pwdRow2?.value
+
+		if pwd1 == pwd2 {
+			Auth.auth().currentUser?.updatePassword(to: pwd1!, completion: { (error) in
+				if error != nil {
+					SnackBarHelper.showError(withText: (error?.localizedDescription)!)
+				} else {
+					SnackBarHelper.showSuccess(withText: "Password successfully updated")
+				}
+
+			})
+		}
+	}
+}
--- EisenhowerTaskManager/EisenhowerTaskManager/Views/SettingsVC/SettingsVC.swift
+++ EisenhowerTaskManager/EisenhowerTaskManager/Views/SettingsVC/SettingsVC.swift
@@ -7,9 +7,45 @@
 //
 
 import UIKit
+import Eureka
+import FirebaseAuth
 
-class SettingsVC: UIViewController {
-    override func viewDidLoad() {
+class SettingsVC: FormViewController {
+
+	override func viewDidLoad() {
         super.viewDidLoad()
+
+		self.title = "Settings"
+
+		form +++ Section("User")
+			<<< ButtonRow {
+				$0.title = "Profile"
+				$0.presentationMode = .show(controllerProvider: .callback(builder: {
+					let profileView = ProfileVC()
+					return profileView
+					}), onDismiss: nil)
+			}
+			+++ Section()
+			<<< ButtonRow {
+				$0.title = "Disconnect"
+				$0.onCellSelection { _, _ in
+					self.disconnect()
+				}
+		}
     }
+}
+
+extension SettingsVC {
+	private func disconnect() {
+		print("Disconnect!")
+		do {
+			try Auth.auth().signOut()
+		} catch let signOutError as NSError {
+			SnackBarHelper.showError(withText: signOutError.localizedDescription)
+			return
+		}
+		if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+			appDelegate.window?.rootViewController = SignInVC()
+		}
+	}
 }
